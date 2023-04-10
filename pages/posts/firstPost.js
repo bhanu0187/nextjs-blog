143b172b13b000e3a1167b00ca0086758f66a427
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-04-10 17:55:53 +0530

    added a route and some styling

--- pages/posts/firstPost.js
+++ pages/posts/firstPost.js
@@ -1,7 +1,23 @@
+import Head from "next/head";
+import Link from "next/link";
+import Script from "next/script";
 import React from "react";
+import Layout from "../../components/Layout";
 
 const FirstPost = () => {
-	return <div>firstPost</div>;
+	return (
+		<Layout>
+			<Head>
+				<title>First Post</title>
+			</Head>
+			<main>
+				<h1>First Post</h1>
+				<h2>
+					<Link href='/'>Back to home</Link>
+				</h2>
+			</main>
+		</Layout>
+	);
 };
 
 export default FirstPost;
